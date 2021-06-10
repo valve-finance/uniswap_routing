9798dbe3df948f7218ccc0316e1063b833caa45c
Alex Carreira <aycarrei@gmail.com>
2021-06-10 16:12:17 -0400

    Server option with express/cors/helmet/http/ratelimiter installed.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import { Command } from 'commander'
 import * as ds from './utils/debugScopes'
 import * as cmds from './commands'
 import * as readline from 'readline'
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import helmet from 'helmet'
+import requestIp from 'request-ip'
+
+const rateLimitMem = require('./middleware/rateLimiterMem.js')
 
 const log = ds.getLog('index')
 const program = new Command();
@@ -174,6 +181,112 @@ const shell = async(): Promise<void> => {
   process.exit(0)
 }
 
+// HTTP Status Codes
+const OK = 200
+const BAD_REQUEST = 400
+const INTERNAL_SERVER_ERROR = 500
+
+const server = async(port: string): Promise<void> => {
+  log.info(`Starting Uniswap Routing Server on port ${port}...\n` +
+           `(wait until 'READY' appears before issuing requests)`)
+
+  const _settings: any = {
+    maxHops: {
+      description: 'The maximum number of hops allowed by the router.',
+      value: 2,
+      type: 'integer'
+    }
+  }
+  let _uniData:any = await initUniData()
+
+  const app = express()
+  app.set('trust proxy', true)
+  app.use(cors())
+  app.use(helmet())
+  app.use(requestIp.mw())
+  if (process.env.NODE_ENV != 'development') {
+    app.use(rateLimitMem)
+  } else {
+    log.warn('Rate limiting disabled in development environment.')
+  }
+  app.use(express.json({limit: '5mb'}))
+  app.use(express.urlencoded({extended: true, limit: '5mb'}))
+
+  app.get(/.*/, async (req:any, res:any) => {
+    res.status(OK).send('Welcome to Uniswap V2 Route Optimization Service.')
+    // TODO: return a message with a link to the API docs for post requests
+  })
+
+  const MAX_HOPS = 3
+  const MAX_RESULTS = 10
+  app.post(/.*/, async (req:any, res:any) => {
+    try {
+      const _startMs = Date.now()
+      const {body} = req
+
+      if (!body.hasOwnProperty('route') && !body.hasOwnProperty('status')) {
+        log.error(`Bad request. Body does not cotain 'route' or 'status' object.`)
+        res.status(BAD_REQUEST).end()
+      }
+
+      const result:any = {}
+
+      const { status } = body
+      if (status) {
+        result.status = 'OK'       
+      }
+
+      const { route } = body
+      if (body) {
+        // TODO: sanity check on route object
+        const {source, dest, amount, options} = route
+        // TODO: support amount and options
+        let maxHops = 3
+        let maxResults = 5
+        log.debug(options)
+        if (options) {
+          if (options.hasOwnProperty('max_hops')) {
+            maxHops = (options.max_hops > 0 && options.max_hops <= MAX_HOPS) ? 
+              options.max_hops : maxHops
+          }
+          if (options.hasOwnProperty('max_results')) {
+            maxResults = (options.max_results > 0 && options.max_results <= MAX_RESULTS) ? 
+              options.max_results : maxResults
+          }
+        }
+        log.debug(`maxHops: ${maxHops}, maxResults: ${maxResults}`)
+        const _rolledRoutes: any = await cmds.findRoutes(_uniData.pairGraph,
+                                                         source,
+                                                         dest,
+                                                         maxHops)
+        const _costedRolledRoutes = cmds.costRolledRoutes(_uniData.numPairData,
+                                                          _rolledRoutes)
+        const _unrolledRoutes = cmds.unrollCostedRolledRoutes(_costedRolledRoutes)
+
+        result.routes = _unrolledRoutes.slice(0, maxResults) // Return the 1st 5 ele
+                                                    // TODO: make this a setting
+      }
+
+      log.debug(`Processed request in ${Date.now() - _startMs} ms`)
+      res.status(OK).json(result)
+    } catch (error) {
+      log.error(error)
+    }
+  })
+ 
+  const _server = http.createServer(app)
+  _server.listen(port, async () => {
+    log.info(`Server on port ${port} READY!`)
+  })
+}
+
+process.on('SIGINT', async():Promise<void> => {
+  log.info('Exiting')
+  // TODO: consider doing this more elegantly (like with tie in to running
+  //       process components).
+  process.exit(0)
+})
+
 program
   .command('main', { isDefault: true })
   .description('Starts an instance of the Uniswap optimizing router, fetching continous updates,' +
@@ -184,6 +297,16 @@ program
   .command('shell')
   .description('Starts a basic shell for exploring Uniswap optimizing router results.')
   .action(shell)
+
+const DEFAULT_PORT = '3030'
+program
+  .command('server [port]')
+  .description('Starts a server that accepts route, status, and other requests. Default port is ' +
+               '3030 and can be overriden with the optional port argment.')
+  .action(async (port) => {
+    port = port || DEFAULT_PORT
+    await server(port)
+  })
 
 program
   .command('help')
---
+++ src/middleware/rateLImiterMem.ts
@@ -0,0 +1,73 @@
+/**                                                                          
+ *  rate-limiter-flexible was chosen for the following reasons:              
+ *                                                                           
+ *  1.  It supports redis but also allows you to use a built in              
+ *      memory store (this makes it easy to deploy the server                
+ *      to AWS and chose to use Elasticache redis while allowing             
+ *      simple local development without having to stand up a                
+ *      redis server / cluster).  Also allows the use of PM2 (our            
+ *      current process manager in AWS as well--see:                         
+ *      https://github.com/animir/node-rate-limiter-flexible/wiki/PM2-cluster
+ *      )                                                                    
+ *                                                                           
+ *  2.  It's performant and robust (see papers on the git page under         
+ *      advantages):                                                         
+ *        - https://github.com/animir/node-rate-limiter-flexible#readme      
+ *                                                                           
+ *  3.  It has fallbacks in the event of failures of redis stores            
+ *                                                                           
+ *  4.  It has configureation flexibility (i.e. limits for different         
+ *      users, ips etc.)                                                     
+ *        - see the copy/paste examples on this wiki:                        
+ *          https://github.com/animir/node-rate-limiter-flexible/wiki/Overall-example#authorized-and-not-authorized-users
+ *                                                                           
+ *  5.  It doesn't have npm audit failures like Brute or others due to       
+ *      race conditions.                                                     
+ *                                                                           
+ *  This middleware code adapted from example here:                          
+ *    - https://github.com/animir/node-rate-limiter-flexible/wiki/Express-Middleware
+ *                                                                           
+ */                           
+import * as ds from './../utils/debugScopes'
+ const log = ds.getLog('rateLimitMem')
+                                                                              
+ const { RateLimiterMemory } = require('rate-limiter-flexible')               
+ 
+ // HTTP Status Codes:
+ const TOO_MANY_REQUESTS = 429
+          
+ // TODO: 
+ //  - redis or other key store.
+ //  - reconsider limits and splitting limites depending on request type (for 
+ //    instance it may be that registerUser only gets called once, but the    
+ //    fetching of notifications might happen more.)  For now 1020 an hour    
+ //    probably suffices for users and might be insufficient for developers.  
+ //  - alarm when rate limit is hit
+ 
+ const rateLimiter = new RateLimiterMemory({                                  
+   keyPrefix: 'middleware',
+   points: 34,   // 34 requests
+   duration: 60   // per minute  (2040 per hour per IP address)               
+ })
+
+// Debug test:  8 / 5 minutes                                                
+// const rateLimiter = new RateLimiterMemory({                               
+//   keyPrefix: 'middleware',
+//   points: 8,                                                              
+//   duration: 5*60
+// })
+
+// Middleware function:                                                      
+const rateLimitMem = (req:any, res:any, next:any) => {                                   
+  const ip = req.clientIp                                                    
+  rateLimiter.consume(ip)   // clientIp comes from request-ip middleware.    
+    .then(() => {
+      next()
+    })
+    .catch(() => {
+      log.warn(`Rate limiting ip address: "${ip}"`)
+      res.status(TOO_MANY_REQUESTS).send('Too Many Requests')
+    })
+}
+
+module.exports = rateLimitMem
